f60cefa3404033a464a9642f89b80791357916cd
Rodrigo <rodrigo_alexandre_rabelo@outlook.com>
2023-01-22 22:29:23 -0300

    feat: Adicionado react-router-dom

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -5,6 +5,12 @@ import Header from "./Header";
 import { createTheme } from '@mui/material/styles';
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -26,10 +32,18 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-      <Container>
-        <Catalog />
-      </Container>
+      <BrowserRouter>
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/catalog/:id" element={<ProductDetails />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,6 +1,13 @@
 import { Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+    const { id } = useParams();
+
+    useEffect(() => {
+        console.log(id);
+    }, []);
     return (
         <Typography variant="h6">
             Product Details
